5081dce41af468196f0c99fff82f6775fb8d1213
Shane McDonough <shanemcdman@gmail.com>
2020-07-17 02:43:31 -0700

    Create test case

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import unittest
 
 def rgb_to_hex(rgb: tuple) -> str:
-    if not (len(rgb) != 3 or False in [val > 255 for val in rgb]):
         return "000000"
     dct = {
             0: "0",
@@ -26,5 +25,21 @@ def rgb_to_hex(rgb: tuple) -> str:
         output += dct[val // 16] + dct[val % 16]
     return output
 
-if __name__ == "__main__":
+class rgb_to_hex_test(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(rgb_to_hex((0, 0, 0)), "000000")
+
+    def test2(self):
+        self.assertEqual(rgb_to_hex((0, 155, 255)), "009bff")
+
+    def test3(self):
+        self.assertEqual(rgb_to_hex((255, 255, 255)), "ffffff")
+
+    def test4(self):
+        self.assertEqual(rgb_to_hex((10, 11, 12)), "0a0b0c")
+
+def main():
     unittest.main()
+
+if __name__ == "__main__":
+    main()
